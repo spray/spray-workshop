5b0ceeced30d55ed1177ffb325f51e87d149bc14
Johannes Rudolph <johannes.rudolph@gmail.com>
2013-10-23 08:53:43 +0200

    Bootstrapping a spray app
     * sbt project
     * simple Service
     * Boot

--- src/main/scala/com/example/Boot.scala
+++ src/main/scala/com/example/Boot.scala
@@ -10,7 +10,7 @@ object Boot extends App {
   implicit val system = ActorSystem("on-spray-can")
 
   // create and start our service actor
-  val service = system.actorOf(Props[MyServiceActor], "demo-service")
+  val service = system.actorOf(Props[MyService], "demo-service")
 
   // start a new HTTP server on port 8080 with our service actor as the handler
   IO(Http) ! Http.Bind(service, interface = "localhost", port = 8080)
