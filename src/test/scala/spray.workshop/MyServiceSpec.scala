633ff69a3c94816779d8d63f439707a146094482
Johannes Rudolph <johannes.rudolph@gmail.com>
2013-10-24 14:24:10 +0200

    step-2b-solution

--- src/test/scala/spray.workshop/MyServiceSpec.scala
+++ src/test/scala/spray.workshop/MyServiceSpec.scala
@@ -3,6 +3,7 @@ package spray.workshop
 import org.specs2.mutable.Specification
 import spray.testkit._
 import spray.http._
+import scala.xml.NodeSeq
 
 class MyServiceSpec extends Specification with Specs2RouteTest with MyService {
   def actorRefFactory = system
@@ -10,8 +11,31 @@ class MyServiceSpec extends Specification with Specs2RouteTest with MyService {
   "MyService" should {
     "return a greeting for GET requests to the root path" in {
       Get() ~> myRoute ~> check {
-        responseAs[String] must contain("Hello world!")
+        responseAs[NodeSeq] ===
+  // format: OFF
+    <html>
+      <body>
+        <div>Hello World</div>
+        <a href="http://xkcd.com/927/"><img src="standards.png"></img></a>
+      </body>
+    </html>
+        // format: ON
       }
     }
+    "greet particular user" in {
+      Get("/users/john/greet") ~> myRoute ~> check {
+        responseAs[String] === "Hello john"
+      }
+    }
+    "do calculation" in {
+      Get("/add/30/58") ~> myRoute ~> check {
+        responseAs[String] === "88"
+      }
+
+      Get("/add/10/1024") ~> myRoute ~> check {
+        responseAs[String] === "1034"
+      }
+    }
+
   }
 }
