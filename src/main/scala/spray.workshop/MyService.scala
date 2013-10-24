03c3b04722fd00ba6cb2fa628c6ccc8d5a496d5b
Johannes Rudolph <johannes.rudolph@gmail.com>
2013-10-24 14:18:59 +0200

    step-2-solution

--- src/main/scala/spray.workshop/MyService.scala
+++ src/main/scala/spray.workshop/MyService.scala
@@ -13,11 +13,24 @@ class MyServiceActor extends Actor with MyService {
 trait MyService extends HttpService {
   // format: OFF
   def myRoute =
-    get {
-      path("") {
+    get(
+      path("")(
         complete(index)
-      }
-    } ~ getFromResourceDirectory("web")
+      ) ~
+      pathPrefix("users")(
+        pathPrefix(Segment)( user =>
+          path("greet")(
+            complete(s"Hello $user")
+          ) ~
+          path("saygoodbye")(
+            complete(helloX(user))
+          )
+        )
+      ) ~
+      path("add" / IntNumber / IntNumber)( (a, b) =>
+        complete((a + b).toString)
+      )
+    ) ~ getFromResourceDirectory("web")
   // format: ON
 
   def index =
@@ -25,6 +38,13 @@ trait MyService extends HttpService {
       <body>
         <div>Hello World</div>
         <a href="http://xkcd.com/927/"><img src="standards.png"></img></a>
+      </body>
+    </html>
+
+  def helloX(user: String) =
+    <html>
+      <body>
+        <div>Goodbye { user }</div>
       </body>
     </html>
 }
