c61945415053324738b8d624540ea929c1d8b6a1
Johannes Rudolph <johannes.rudolph@gmail.com>
2013-10-25 10:50:47 +0200

    step-3a-solution

--- src/main/scala/spray.workshop/MyService.scala
+++ src/main/scala/spray.workshop/MyService.scala
@@ -29,7 +29,12 @@ trait MyService extends HttpService {
       ) ~
       path("add" / IntNumber / IntNumber)( (a, b) =>
         complete((a + b).toString)
-      )
+      ) ~
+      path("add") {
+        parameters('a.as[Int], 'b.as[Int]) { (a, b) =>
+          complete((a + b).toString)
+        }
+      }
     ) ~ getFromResourceDirectory("web")
   // format: ON
 
@@ -38,6 +43,12 @@ trait MyService extends HttpService {
       <body>
         <div>Hello World</div>
         <a href="http://xkcd.com/927/"><img src="standards.png"></img></a>
+        <h2>Calculator</h2>
+        <form action="/add">
+          <div>a: <input type="text" name="a"></input></div>
+          <div>b: <input type="text" name="b"></input></div>
+          <div><input type="submit" value="add"></input></div>
+        </form>
       </body>
     </html>
 
